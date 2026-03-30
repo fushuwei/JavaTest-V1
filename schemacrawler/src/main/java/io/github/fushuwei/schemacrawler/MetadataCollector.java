5b2226d70cc183805d21108533319bf7ef1b2476
fushuwei <331044761@qq.com>
2026-03-30 14:07:42 +0800

    提交修改

--- schemacrawler/src/main/java/io/github/fushuwei/schemacrawler/MetadataCollector.java
+++ schemacrawler/src/main/java/io/github/fushuwei/schemacrawler/MetadataCollector.java
@@ -5,6 +5,7 @@ import schemacrawler.schema.Column;
 import schemacrawler.schema.ForeignKey;
 import schemacrawler.schema.Schema;
 import schemacrawler.schema.Table;
+import schemacrawler.schemacrawler.LimitOptionsBuilder;
 import schemacrawler.schemacrawler.SchemaCrawlerOptions;
 import schemacrawler.schemacrawler.SchemaCrawlerOptionsBuilder;
 import schemacrawler.tools.utility.SchemaCrawlerUtility;
@@ -19,11 +20,17 @@ public class MetadataCollector {
 
     public static void main(String[] args) throws Exception {
         // 1. 配置数据库连接
-        String url = "jdbc:mysql://localhost:3306/nacos";
+        String url = "jdbc:mysql://localhost:3306/sca_platform";
         try (Connection connection = DriverManager.getConnection(url, "root", "123456")) {
 
-            // 2. 配置SchemaCrawler选项
-            SchemaCrawlerOptions options = SchemaCrawlerOptionsBuilder.newSchemaCrawlerOptions();
+            // 2. 配置SchemaCrawler选项，只采集指定的数据库，从JDBC URL中提取数据库名称
+            String dbName = url.substring(url.lastIndexOf("/") + 1).split("\\?")[0];
+
+            LimitOptionsBuilder limitOptionsBuilder = LimitOptionsBuilder.builder()
+                .includeSchemas(schema -> schema.equals(dbName));
+
+            SchemaCrawlerOptions options = SchemaCrawlerOptionsBuilder.newSchemaCrawlerOptions()
+                .withLimitOptions(limitOptionsBuilder.toOptions());
 
             // 3. 执行采集，获取Catalog对象（这是核心数据载体）
             DatabaseConnectionSource connectionSource = DatabaseConnectionSources.fromConnection(connection);
@@ -38,7 +45,7 @@ public class MetadataCollector {
                     // 获取列信息
                     for (Column column : table.getColumns()) {
                         System.out.println("    Column: " + column.getName()
-                            + " (" + column.getColumnDataType() + ")");
+                            + " (" + column.getAttributes().get("REMARKS") + "    " + column.getColumnDataType() + ")");
                     }
                     // 获取主键信息
                     if (table.getPrimaryKey() != null) {
